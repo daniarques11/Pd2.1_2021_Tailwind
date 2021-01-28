923683639ca14b5d8e16ff311f01b3eac1ecda06
Albertomanas <amanas@cifpfbmoll.eu>
2021-01-28 18:04:24 +0100

    fix(band): add background color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
         extend: {
             height: {
                 "60vh": "60vh"
+            },
+            background: {
+                "#f1f1f1": "#f1f1f1"
             }
         },
     },
